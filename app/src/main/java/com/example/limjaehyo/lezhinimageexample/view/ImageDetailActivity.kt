78415425631753cd56e34958c0a4282cb4d2792a
임재효 <exit1190@naver.com>
2018-09-12 11:43:28 +0900

    이미지 확대 추가

--- app/src/main/java/com/example/limjaehyo/lezhinimageexample/view/ImageDetailActivity.kt
+++ app/src/main/java/com/example/limjaehyo/lezhinimageexample/view/ImageDetailActivity.kt
@@ -1,15 +1,13 @@
 package com.example.limjaehyo.lezhinimageexample.view
 
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
 import com.example.limjaehyo.lezhinimageexample.R
 import com.example.limjaehyo.lezhinimageexample.model.datasource.ImageQueryModel
 import com.example.limjaehyo.lezhinimageexample.util.CommonUtil
 import com.facebook.drawee.backends.pipeline.Fresco
-import com.facebook.drawee.view.SimpleDraweeView
 import me.relex.photodraweeview.PhotoDraweeView
 import java.util.*
 
@@ -22,6 +20,7 @@ class ImageDetailActivity : AppCompatActivity() {
         val parcelable = intent.getParcelableExtra<ImageQueryModel.Documents>("item")
 
         val photoDraweeView = findViewById<PhotoDraweeView>(R.id.my_image_view)
+
         photoDraweeView.setPhotoUri(Uri.parse(parcelable.image_url))
         val ratioHeight = CommonUtil.getRatioHeight(this,parcelable.height.toInt(),parcelable.width.toInt())
         val layoutParams = photoDraweeView.layoutParams
